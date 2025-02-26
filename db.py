39f46f6d3077543ac080043e80978bbc94d46783
redream <shykalist@mail.ru>
2025-02-27 01:07:42 +0300

    2nd tab + fixes

--- db.py
+++ db.py
@@ -9,28 +9,27 @@ class DB:
 
         self.connection = sqlite3.connect(database, check_same_thread = False)
         self.cursor = self.connection.cursor()
+
+        self.cursor.execute('PRAGMA foreign_keys = ON;')
 			
     def get_dl(self, id):
-        with self.connection:
-            with lock:
-                result = self.cursor.execute("""SELECT dls.*, group_concat(categories.name) AS categories FROM dls
-                                                JOIN dls_categories
-                                                ON dls_categories.dl_id = dls.id
-                                                JOIN categories
-                                                ON categories.id = dls_categories.category_id WHERE dls.id = ?
-                                                GROUP BY dls.id""", (id,)).fetchone()
-                return result
+        with self.connection:   
+            result = self.cursor.execute("""SELECT dls.*, group_concat(categories.name) AS categories FROM dls
+                                            JOIN dls_categories
+                                            ON dls_categories.dl_id = dls.id
+                                            JOIN categories
+                                            ON categories.id = dls_categories.category_id WHERE dls.id = ?
+                                            GROUP BY dls.id""", (id,)).fetchone()
+            return result
             
     def get_dl_tickets(self, id):
         with self.connection:
-            with lock:
-                result = self.cursor.execute("""SELECT tickets.date, tickets.id, tickets.type, tickets.amount, district_departments.name as department_name
-                                                FROM tickets 
-                                                JOIN district_departments ON tickets.district_id = district_departments.id
-                                                WHERE tickets.dl_id = ? ORDER BY DATETIME(tickets.date);""", (id,)).fetchall()
-                return result
+            result = self.cursor.execute("""SELECT tickets.date, tickets.id, tickets.type, tickets.amount, district_departments.name as department_name
+                                            FROM tickets 
+                                            JOIN district_departments ON tickets.district_id = district_departments.id
+                                            WHERE tickets.dl_id = ? ORDER BY DATETIME(tickets.date);""", (id,)).fetchall()
+            return result
 
-    def set_status(self, uid, status):
+    def add_ticket(self, distcrict_id, dl_id, reason, date, amount):
         with self.connection:
-            with lock:
-                return self.cursor.execute("UPDATE `user` SET `status` = ? WHERE `uid` = ?", (status, uid))
+            return self.cursor.execute("INSERT INTO tickets (district_id, dl_id, type, date, amount) VALUES (?, ?, ?, ?, ?)", (distcrict_id, dl_id, reason, date, amount))
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from tkinter import ttk
 from db import DB
 from tkinter.messagebox import showerror, showwarning, showinfo
 from tkinter import font
+from datetime import datetime
 
 #------ INIT ------#
 
@@ -20,8 +21,6 @@ root.option_add("*Font", font_normal)
 
 font_bold = font.Font(family= "Segoe UI Semibold", size=10, weight="normal", slant="roman")
 
-
-
 # Создание Notebook (вкладок)
 notebook = ttk.Notebook(root)
 notebook.pack(expand=True, fill=BOTH, padx=3, pady=5)
@@ -29,19 +28,23 @@ notebook.pack(expand=True, fill=BOTH, padx=3, pady=5)
 #------ Функции и переменные ------#
 
 entry_dl_id = StringVar(value='')
+insert_dl_id = StringVar(value='')
+insert_district_id = StringVar(value='')
+insert_reason = StringVar(value='')
+insert_amount = StringVar(value='')
 confirmed = {'dl_id': StringVar(value='№В/У: '), 
              'fio': StringVar(value='ФИО: '),
              'issue_date': StringVar(value='Дата выдачи: '),
              'categories': StringVar(value='Категории: ')}
 
-def click_handler():
+def get_dl_info():
     global confirmed, entry_dl_id, tree
     dl_id = entry_dl_id.get()
 
     driverinfo = db.get_dl(dl_id)
 
     if driverinfo == None:
-        showerror(title="Ошибка запроса", message=f"Нет В/У с номером: {dl_id}")
+        showerror(title="Ошибка запроса", message=f"Не найдено В/У с номером: {dl_id}")
         return
     
     confirmed['dl_id'].set(f"№В/У: {dl_id}")
@@ -57,6 +60,25 @@ def click_handler():
     for ticket in tickets:
         tree.insert("", END, values=ticket)
 
+def insert_ticket():
+    global insert_dl_id, insert_district_id, insert_reason, insert_amount
+
+    if db.get_dl(insert_dl_id.get()) == None:
+        showerror(title="Ошибка запроса", message=f"Не найдено В/У с номером: {insert_dl_id.get()}")
+        return
+    
+
+    date = datetime.now().strftime('%Y-%m-%d')
+
+    try:
+        res = db.add_ticket(insert_district_id.get(), insert_dl_id.get(), insert_reason.get(), date, insert_amount.get())
+        if res:
+            showinfo(title="Внесение штрафа", message="Штраф был успешно внесен!")
+
+    except Exception as e:
+        showerror(title="Ошибка запроса", message='Неверно указан код района/сумма штрафа!')
+        return
+
 
 #------ Вкладка 1: Информация о В/У ------#
 
@@ -65,7 +87,7 @@ notebook.add(tab1, text="Информация о В/У")
 
 ttk.Label(tab1, text='Укажите номер В/У:').pack(anchor=NW, pady=3)
 ttk.Entry(tab1, textvariable=entry_dl_id).pack(anchor=NW, pady=3)
-ttk.Button(tab1, text='Проверить', command=click_handler).pack(anchor=NW, pady=3)
+ttk.Button(tab1, text='Проверить', command=get_dl_info).pack(anchor=NW, pady=3)
 
 ttk.Label(tab1, text='').pack(anchor=NW, pady=2)
 
@@ -95,13 +117,28 @@ tree.column('#4', stretch=NO, width=80, anchor=CENTER)
 tree.column('#5', stretch=YES, width=300)
 
 
-#------ Вкладка 2: Дополнительная информация ------#
+#------ Вкладка 2: Внесение штрафа ------#
 
 tab2 = ttk.Frame(notebook)
-notebook.add(tab2, text="Дополнительная информация")
+notebook.add(tab2, text="Внесение штрафа")
 
 # Добавление виджетов на вторую вкладку
-ttk.Label(tab2, text="Это вторая вкладка").pack(padx=10, pady=10)
+ttk.Label(tab2, text='Заполните данные:', font=font_bold).grid(column=0, row=0, pady=10, padx=10, sticky=W)
+
+ttk.Entry(tab2, textvariable=insert_dl_id).grid(column=0, row=1, padx=10)
+ttk.Label(tab2, text='Номер В/У').grid(column=0, row=2)
+
+ttk.Entry(tab2, textvariable=insert_district_id).grid(column=1, row=1, padx=10)
+ttk.Label(tab2, text='Код района').grid(column=1, row=2, padx=10)
+
+ttk.Entry(tab2, textvariable=insert_reason).grid(column=2, row=1, padx=10)
+ttk.Label(tab2, text='Причина').grid(column=2, row=2, padx=10)
+
+ttk.Entry(tab2, textvariable=insert_amount).grid(column=3, row=1, padx=10)
+ttk.Label(tab2, text='Сумма').grid(column=3, row=2, padx=10)
+
+ttk.Button(tab2, text='Внести', command=insert_ticket).grid(column=4, row=1, padx=20, rowspan=2)
+
 
 #------ Запуск основного цикла ------#
 
